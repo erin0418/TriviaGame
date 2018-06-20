9d1fc2de934ff3b2e83f3fccfc64ed643108523d
Erin0418 <erinf0418@gmail.com>
2018-06-19 23:32:23 -0400

    questions added

---
+++ assests/javascript/app.js
@@ -0,0 +1,33 @@
+var questions = [{
+    question: "When did the Philadelphia Flyers join the NHL?",
+    answers: [1965, 1967, 1973, 1982],
+    correct: 1,
+},
+{
+    question: "Who is the team's all time highest goal scorer?",
+    answers: ["Bobby Clarke", "Eric Lindros", "Reggie Leech", "Claude Giroux"],
+    correct: 2,
+},
+{
+    question: "Which of these numbers is a retired player's number?",
+    answers: [18, 4, 5, 6],
+    correct: 3,
+},
+{
+    question: "Where did the Flyers win their last Stanley Cup?",
+    answers: ["Philadelphia", "Buffalo", "Montreal", "New York"],
+    correct: 1,
+},
+{
+    question: "How many goals has goalie Ron Hextall scored?",
+    answers: [0, 2, 1, 3],
+    correct: 1,
+},
+{
+    question: "What team did the Flyers notoriously beat in the 1970's?",
+    answers: ["Montreal Canadiens", "The Globe Trotters", "New York Rangers", "The Red Army"],
+    correct: 3,
+},
+]
+
+$("#.container").html(questions);
